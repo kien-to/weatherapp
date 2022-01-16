46716ca6e8c05e467c7794b8b340976106b5a88d
lk thai ha <lkthaiha@macbook-air.ddns.umass.edu>
2022-01-16 14:35:28 -0500

    New commit message

--- src/App.js
+++ src/App.js
@@ -1,25 +1,118 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react'
+import axios from 'axios'
 
-function App() {
+const Filter = ({handleFilter, newFilter}) => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div> 
+        Filter (case-insensitive): <input value={newFilter} onChange ={handleFilter}/>
     </div>
-  );
+  )
+}
+const Weather = ({ weather, city }) => {
+  console.log(weather)
+  if (!weather) {
+    return null
+  }
+
+  return (
+    <div>
+      <h3>Weather in {city}</h3>
+      <div>
+        <strong>Temperature:</strong> {weather.main.temp} Celcius
+      </div>
+      <div><strong>Weather description:</strong> {weather.weather[0].description} </div> 
+      <div>
+        <strong>Wind:</strong> {weather.wind.speed} mph direction {weather.wind.deg}
+      </div>
+      
+    </div>
+  )
 }
 
-export default App;
+const Country = ({ country }) => {
+  const [weather, setWeather] = useState(null)
+  const api_key = process.env.REACT_APP_API_KEY
+  //const url = `http://api.weatherstack.com/current?access_key=${api_key}&query=${country.capital}`
+  const url = `http://api.openweathermap.org/data/2.5/weather?q=${country.capital}&appid=${api_key}`
+  useEffect(() => {
+    axios.get(url).then(response => {
+      setWeather(response.data)
+    })
+  }, [url])
+
+  return (
+    <div>
+      <h2>{country.name.common}</h2>
+
+      <div>capital {country.capital}</div>
+      <div>population {country.population}</div>
+
+      <div>
+        <h1>Flag</h1>
+        {country.flag}
+      </div> 
+      <Weather weather={weather} city={country.capital} />
+    </div>
+  )
+}
+
+const Countries = ({countries}) => {
+  if (countries.length === 1) {
+    return(
+      <Country country = {countries[0]}/>
+    )}
+  else if (countries.length < 10) {
+  return(
+    <div>
+      {countries.map(c =>
+        <div key={c.alpha2Code}>
+          {c.name.common}
+          <div>
+          <h3>Flag {c.flag}</h3>
+          </div> 
+        </div>
+      )}
+    </div>
+  )}
+  return (
+    <div>
+      Too many matches, specify another filter
+    </div>
+  )
+}
+
+const App = () => {
+
+  const [countries, setCountries] = useState([]) 
+  const [newFilter, setFilter] = useState('')
+
+  useEffect(() => {
+    axios
+      .get('https://restcountries.com/v3.1/all')
+      .then(response => {
+        console.log('promise fulfilled')
+        setCountries(response.data)
+      })
+  }, [])
+
+  const handleFilter = (event) => {
+    setFilter(event.target.value)
+  }
+
+  const filteredCountries = countries.filter(c => c.name.common.toLowerCase().includes(newFilter.toLowerCase()))
+
+  return (
+    <div>
+      <h2>Countries</h2>
+      <Filter 
+        handleFilter = {handleFilter}
+        newFilter = {newFilter}
+      />
+      <Countries 
+        countries = {filteredCountries} 
+        />
+    </div>
+  )
+}
+
+export default App
